4eda4044bd4e17957ac23ac9e72f2c3ed96a973e
Disha Bhamare <dishabhamare008@gmail.com>
2026-01-16 22:35:36 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,11 @@
+function topfunction(){
+    document.body.scrollTop=0;
+    document.documentElement.scrollTop=0;
+    
+}
+function scrollfunction(){
+    document.body.scrollDown;
+    document.documentElement.scrollDown=0;
+    
+}
+
